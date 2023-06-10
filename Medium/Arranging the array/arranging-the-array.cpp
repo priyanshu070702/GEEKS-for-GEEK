55defde4705b0159fda9ba2ced7ffc7c86f9cd37
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-06-10 17:03:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Arranging the array/arranging-the-array.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n){
+            //approach-1
+            int A[n];
+            int j=0;
+            for(int i=0;i<n;i++){
+                if(arr[i]<0){
+                    A[j++]=arr[i];
+                }
+            }
+            for(int i=0;i<n;i++){
+                if(arr[i]>=0){
+                    A[j++]=arr[i];
+                }
+            }
+            for(int i=0;i<n;i++){
+                arr[i]=A[i];
+            }
+        }
+};
+
+//{ Driver Code Starts.
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+        Solution ob;
+        ob.Rearrange(arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} 
+// } Driver Code Ends
