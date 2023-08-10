b63343b7054cd65af9c25935ff28a20b6b57a7d0
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-08-10 12:42:49 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -10,28 +10,32 @@ class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    int solveMemo(int n, int m, string s1, string s2, int i, int j, vector<vector<int>>&dp){
-        if(i==n||j==m)return 0;
+    int solveRec(string s1, string s2, int i, int j, vector<vector<int>>&dp){
+        if(i==s1.length() || j==s2.length()){
+            return 0;
+        }
         if(dp[i][j]!=-1)return dp[i][j];
         int len=0;
         if(s1[i]==s2[j]){
-            len=1+solveMemo(n,m,s1,s2,i+1,j+1,dp);
+            len=1+solveRec(s1,s2,i+1,j+1,dp);
         }
         else{
-            len=max(solveMemo(n,m,s1,s2,i+1,j,dp),solveMemo(n,m,s1,s2,i,j+1,dp));
+            len=max(solveRec(s1,s2,i,j+1,dp),solveRec(s1,s2,i+1,j,dp));
         }
         return dp[i][j]=len;
     }
-    int solveTab(int n, int m, string s1, string s2){
+    int solveRec(string s1, string s2){
+        int n=s1.length();
+        int m=s2.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
                 int len=0;
                 if(s1[i]==s2[j]){
                     len=1+dp[i+1][j+1];
-                } 
+                }
                 else{
-                    len=max(dp[i+1][j],dp[i][j+1]);
+                    len=max(dp[i][j+1],dp[i+1][j]);
                 }
                 dp[i][j]=len;
             }
@@ -41,8 +45,8 @@ class Solution
     int lcs(int n, int m, string s1, string s2)
     {
         // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        // return solveMemo(n,m,s1,s2,0,0,dp);
-        return solveTab(n,m,s1,s2);
+        // return solveRec(s1,s2,0,0,dp);
+        return solveRec(s1,s2);
     }
 };
 
