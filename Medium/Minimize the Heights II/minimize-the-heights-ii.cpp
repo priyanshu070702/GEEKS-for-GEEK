9251736d90d5e1842aed02b2bf4ffaac061021b5
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-12-05 15:52:01 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
+++ Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -11,15 +11,15 @@ class Solution {
   public:
     int getMinDiff(int arr[], int n, int k) {
         sort(arr,arr+n);
-        int diff=arr[n-1]-arr[0];
-        int mx,mn; 
+        int ans=arr[n-1]-arr[0];
+        int mn,mx;
         for(int i=1;i<n;i++){
             if(arr[i]-k<0)continue;
-            mx=max(arr[i-1]+k,arr[n-1]-k);
+            mx=max(arr[n-1]-k,arr[i-1]+k);
             mn=min(arr[0]+k,arr[i]-k);
-            diff=min(diff,mx-mn);
+            ans=min(ans,(mx-mn));
         }
-        return diff;
+        return ans;
     }
 };
 
