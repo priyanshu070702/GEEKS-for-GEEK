e527be96be542e36e43b91cd47dea2d4b1f21414
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2024-04-11 12:25:34 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Party of Couples/party-of-couples.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int findSingle(int n, int arr[]){
+        sort(arr,arr+n);
+        for(int i=0;i<n-1;i=i+2){
+            if(arr[i]!=arr[i+1])return arr[i];
+        }
+        return arr[n-1];
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, X;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.findSingle(N, arr) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
