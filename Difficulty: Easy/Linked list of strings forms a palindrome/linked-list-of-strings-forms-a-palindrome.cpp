09d55b44c582431145338b6626ecadba3352c914
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2024-07-02 19:49:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked list of strings forms a palindrome/linked-list-of-strings-forms-a-palindrome.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+// Code by Shubham Joshi
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    string data;
+    Node *next;
+
+    Node(string x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *newNode(string str) {
+    Node *new_node = new Node(str);
+    new_node->next = NULL;
+    return new_node;
+}
+
+void print(Node *root) {
+    Node *temp = root;
+
+    while (temp != NULL) {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+struct Node
+{
+    string data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    bool isPal(string st){
+        if(st=="")return 1;
+        int i=0,j=st.length()-1;
+        while(i<j){
+            if(st[i++]!=st[j--])return 0;
+        }
+        return 1;
+    }
+    bool compute(Node* head) {
+        string ans="";
+        while(head!=NULL){
+            ans+=head->data;
+            head=head->next;
+        }
+        return isPal(ans);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    // your code goes here
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int K;
+        cin >> K;
+        struct Node *head = NULL;
+        struct Node *temp = head;
+
+        for (int i = 0; i < K; i++) {
+            string str = "";
+            cin >> str;
+            if (head == NULL)
+                head = temp = new Node(str);
+            else {
+                temp->next = new Node(str);
+                temp = temp->next;
+            }
+        }
+        Solution ob;
+        bool ans = ob.compute(head);
+        if (ans)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
